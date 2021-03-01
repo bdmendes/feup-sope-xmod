144e7af8e78f7c5809d932172c6f2838dbc097bc
Bruno Mendes <brod56@sapo.pt>
2021-03-01 10:42:24 +0000

    Add program name constant

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
+static const char *PROGRAM_NAME = "xmod";
+// Continue with options, perhaps in a header file
+
 int main(int argc, char **argv) {
   for (int i = 0; i < argc; ++i) {
     printf("Argument %d: %s\n", i, argv[i]);
-  }
-  for (int i = 0; i < 2; ++i) {
-    printf("dude");
   }
   return 0;
 }
