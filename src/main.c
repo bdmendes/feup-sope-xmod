462632415113dde1059858ae821d4a96f2e42cb9
Bruno Mendes <brod56@sapo.pt>
2021-03-04 09:29:05 +0000

    Fix main typo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ static const char *PROGRAM_NAME = "xmod";
 int main(int argc, char **argv) {
   for (int i = 0; i < argc; ++i) {
     printf("Argument %d: %s\n", i, argv[i]);
-    pp
   }
   return 0;
 }
