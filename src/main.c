3ac6004af87109ef6f791805bee2eab64d831a46
Bruno Mendes <brod56@sapo.pt>
2021-03-05 10:44:36 +0000

    Format with 4 spaces by default

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@ static const char *PROGRAM_NAME = "xmod";
 // Continue with options, perhaps in a header file
 
 int main(int argc, char **argv) {
-  for (int i = 0; i < argc; ++i) {
-    printf("Argument %d: %s\n", i, argv[i]);
-  }
-  return 0;
+    for (int i = 0; i < argc; ++i) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+    return 0;
 }
