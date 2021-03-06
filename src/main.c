83cadd6ede2247325d8958b693b98834290f5666
eukia <ritam.mendes@gmail.com>
2021-03-06 01:07:02 +0000

    Change fstat to fstatat

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <fts.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -7,29 +8,24 @@
 static const char *PROGRAM_NAME = "xmod";
 // Continue with options, perhaps in a header file
 
-/**
- * @brief reads the current mode of a file
- *
- * @param file_descriptor the descriptor correspondent to the file
- * @param old_mode the mode read
- * @return int 0 on success
- */
-int read_old_mode(int file_descriptor, mode_t *old_mode);
+int retrieve_file_mode(int dir_fd, const char *path_name, mode_t *file_mode,
+                       int flag);
 
 int main(int argc, char **argv) {
-  int file = open(argv[1], O_RDONLY);
-  mode_t mode = 0;
-  read_old_mode(file, &mode);
-  printf("Old mode: %o\n", mode);
-  return 0;
+    int dirfd = open(argv[1], O_RDONLY);
+    mode_t mode = 0;
+    retrieve_file_mode(dirfd, argv[2], &mode, 0);
+    printf("Old mode: %o\n", mode);
+    return 0;
 }
 
-int read_old_mode(int file_descriptor, mode_t *old_mode) {
-  struct stat sb;
-  if (fstat(file_descriptor, &sb) < 0) {
-    perror("fstat");
+int retrieve_file_mode(int dir_fd, const char *path_name, mode_t *file_mode,
+                       int flag) {
+    struct stat sb;
+    if (fstatat(dir_fd, path_name, &sb, flag) < 0) {
+        perror("fstat");
+        return 0;
+    }
+    *file_mode = sb.st_mode;
     return 0;
-  }
-  *old_mode = sb.st_mode;
-  return 0;
 }
