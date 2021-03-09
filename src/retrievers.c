56b8627fb4625d7d6f8c4ef41d40db5e22500e1c
eukia <ritam.mendes@gmail.com>
2021-03-09 23:53:01 +0000

    Make file info retriever aux functions static

--- src/retrievers.c
+++ src/retrievers.c
@@ -3,14 +3,19 @@
 #include "retrievers.h"
 #include "xmod.h"
 
+static void assemble_file_info(FileInfo *file_info, const mode_t mode,
+                               const char *path);
+
+static int retrieve_file_mode(const char *path_name, mode_t *file_mode);
+
 void retrieve_file_info(FileInfo *file_info, char *file_path) {
     mode_t mode = 0;
     retrieve_file_mode(file_path, &mode);
     assemble_file_info(file_info, mode, file_path);
 }
 
-void assemble_file_info(FileInfo *file_info, const mode_t mode,
-                        const char *path) {
+static void assemble_file_info(FileInfo *file_info, const mode_t mode,
+                               const char *path) {
     file_info->path = path;
     file_info->octal_mode = mode & 0777;
 
@@ -32,7 +37,7 @@ void assemble_file_info(FileInfo *file_info, const mode_t mode,
         file_info->file_type = -1;
 }
 
-int retrieve_file_mode(const char *path_name, mode_t *file_mode) {
+static int retrieve_file_mode(const char *path_name, mode_t *file_mode) {
     struct stat sb;
     if (stat(path_name, &sb) < 0) {
         perror("stat");
--- src/retrievers.h
+++ src/retrievers.h
@@ -6,8 +6,4 @@
 
 void retrieve_file_info(FileInfo *file_info, char *file_path);
 
-int retrieve_file_mode(const char *path_name, mode_t *file_mode);
-
-void assemble_file_info(FileInfo *file_info, const mode_t mode, const char *path);
-
 #endif
