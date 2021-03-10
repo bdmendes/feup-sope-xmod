0abeb654398e28857a99165dcf9b7db0a8a52e80
eukia <ritam.mendes@gmail.com>
2021-03-10 23:00:43 +0000

    Fix ignoring special files

--- src/transverse.c
+++ src/transverse.c
@@ -11,15 +11,15 @@ void transverse_dir(DIR *dp) {
     struct dirent *dirent;
 
     while ((dirent = readdir(dp)) != NULL) {
-        if (dirent->d_type == DT_REG) {
-            printf("Regular file : %s\n", dirent->d_name);
-            // to things to regual files
-        } else if (dirent->d_type == DT_DIR) {
+        if (dirent->d_type == DT_DIR) {
             if (strcmp(dirent->d_name, ".") != 0 &&
                 strcmp(dirent->d_name, "..") != 0) {
                 // recurse
                 printf("Directory : %s\n", dirent->d_name);
             }
+        } else {
+            printf("File : %s\n", dirent->d_name);
+            // do things to regual files
         }
     }
 }
