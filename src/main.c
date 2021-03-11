2d90b37a5e0c7fabcdcff0a79d552a3968e60681
Bruno Mendes <brod56@sapo.pt>
2021-03-11 19:24:42 +0000

    Convert to exec call

--- src/main.c
+++ src/main.c
@@ -7,18 +7,27 @@
 #include "retrievers.h"
 #include "utils.h"
 
-int transverse(XmodCommand *cmd) {
-    char *curr_path = cmd->file_dir;
+int main(int argc, char **argv) {
+    char str[] = "testdir";
+
+    // Hard coded command content. Waiting for parser
+    XmodCommand cmd;
+    strcpy(cmd.file_dir, str);
+    cmd.mode.octal_mode = 0744;
+    cmd.mode_type = OCTAL_MODE;
+    cmd.options.recursive = true;
+
+    char *curr_path = cmd.file_dir;
     printf("Changing file permissions: %s\n", curr_path);
     FileInfo file_info;
     if (retrieve_file_info(&file_info, curr_path) != 0) {
-        printf("Stat failed!!!!\n");
+        perror("could not retrieve file info\n");
         return -1;
     }
-    chmod(curr_path, cmd->mode.octal_mode);
+    chmod(curr_path, cmd.mode.octal_mode);
 
-    if (cmd->options.recursive && file_info.type == DT_DIR) {
-        DIR *dp = opendir(cmd->file_dir);
+    if (cmd.options.recursive && file_info.type == DT_DIR) {
+        DIR *dp = opendir(cmd.file_dir);
         struct dirent *dirent;
 
         while ((dirent = readdir(dp)) != NULL) {
@@ -26,32 +35,27 @@ int transverse(XmodCommand *cmd) {
                 if (strcmp(dirent->d_name, ".") != 0 &&
                     strcmp(dirent->d_name, "..") != 0) {
                     printf("Entering directory : %s\n", dirent->d_name);
+
                     pid_t i = fork();
                     if (i == 0) {
                         char dash[] = "/";
-                        strcpy(cmd->file_dir,
-                               strcat(strcat(curr_path, dash), dirent->d_name));
-                        sleep(3);
-                        transverse(cmd);
+                        for (int i = 0; i < argc; ++i) {
+                            if (argv[i] == cmd.file_dir) {
+                                strcpy(cmd.file_dir,
+                                       strcat(strcat(curr_path, dash),
+                                              dirent->d_name));
+                                argv[i] = cmd.file_dir;
+                                break;
+                            }
+                        }
+                        execvp("xmod", argv);
                     }
                 }
             } else {
                 printf("Changing file permissions: %s\n", dirent->d_name);
-                sleep(3);
-                chmod(curr_path, cmd->mode.octal_mode);
+                chmod(curr_path, cmd.mode.octal_mode);
             }
         }
     }
     return 0;
-}
-
-int main(int argc, char **argv) {
-    char str[] = "testdir";
-
-    XmodCommand cmd;
-    strcpy(cmd.file_dir, str);
-    cmd.mode.octal_mode = 0744;
-    cmd.mode_type = OCTAL_MODE;
-    cmd.options.recursive = true;
-    transverse(&cmd);
 }
