41b8d37e25e2c3efc0cd47c9d0c1ced0a8dda2a9
kat-maq <laimirandasantos@gmail.com>
2021-03-14 20:18:42 +0000

    Add octal_mode invalid function

--- src/input_validation.c
+++ src/input_validation.c
@@ -2,10 +2,22 @@
 #include "utils.h"
 
 #include <stdio.h>
+#include <string.h>
 
 static bool is_invalid_flag(char *argument){
 
     return !is_flag_arg(argument) && !has_permissions_flags(argument);
+}
+
+static bool is_invalid_octal_number(const char *octal_number){
+    int counter_octal_digits = 0;
+
+    for(int i = strlen(octal_number) - 1; i >= 0; --i){
+        if(octal_number[i] > '7') return true;
+        counter_octal_digits++;
+        if(counter_octal_digits > 3 && octal_number[i] != '0') return true;
+    }
+    return false;
 }
 
 bool is_invalid_input(char** argv, int argc){
@@ -18,5 +30,10 @@ bool is_invalid_input(char** argv, int argc){
         }
         mode_index++;
     }
+    if(is_number_arg(argv[mode_index]) && is_invalid_octal_number(argv[mode_index])){
+        printf("xmod: invalid mode '%s'\n", argv[mode_index]);
+        return true;
+    }
+
     return false;
 }
