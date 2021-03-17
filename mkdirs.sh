5d706a4d1a5b2ee005d67c867abf2eb92929268f
eukia <ritam.mendes@gmail.com>
2021-03-17 23:24:50 +0000

    Fix scripts

--- mkdirs.sh
+++ mkdirs.sh
@@ -7,28 +7,26 @@ FILE_NAME1="mike"
 FILE_NAME2="pp"
 FILE_NAME3="ahm"
 
-cd testdir
-
 mkdir $DIR_NAME
 cd $DIR_NAME
 for i in {1..2000}
 do
-    mkdir -p "$DIR_NAME - $i"
-    touch "$FILE_NAME1 - $i"
+    mkdir -p "$DIR_NAME-$i"
+    touch "$FILE_NAME1-$i"
 done
 
-cd "$DIR_NAME - 1"
+cd "$DIR_NAME-1"
 for i in {1..2000}
 do
-    mkdir -p "$DIR_NAME1 - $i"
-    touch "$FILE_NAME2 - $i"
+    mkdir -p "$DIR_NAME1-$i"
+    touch "$FILE_NAME2-$i"
 done
 
-cd "$DIR_NAME1 - 1"
+cd "$DIR_NAME1-1"
 for i in {1..2000}
 do
-    mkdir -p "$DIR_NAME2 - $i"
-    touch "$FILE_NAME3 - $i"
+    mkdir -p "$DIR_NAME2-$i"
+    touch "$FILE_NAME3-$i"
 done
 
 mkdir -p $DIR_NAME2
