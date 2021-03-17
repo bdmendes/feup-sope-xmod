fd4e361c166703f8dbd51a735d13ad0eb7d7286b
eukia <ritam.mendes@gmail.com>
2021-03-17 22:23:55 +0000

    Document verbose module

--- src/verbose/verbose.h
+++ src/verbose/verbose.h
@@ -4,13 +4,33 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
+/**
+ * @brief the three kinds of user that can interact with a file (the one who
+ * created it, the ones in the same group, others)
+ *
+ */
 enum XMOD_USER_TYPE { OWNER, GROUP, OTHER };
 
+/**
+ * @brief
+ *
+ */
 enum XMOD_PERMISSION { READ, WRITE, EXECUTE };
 
 #define NUMBER_OF_PERM_FIELDS 9
 
+/**
+ * @brief Print to standard output the corresponding message (mode changed, mode
+ * retained, call failed) with the file's permission modes' old and new values.
+ *
+ * @param file_name name of the file
+ * @param old_mode permission mode before the chmod call
+ * @param new_mode permission mode after the chmod call
+ * @param changed whether or not the file's permission mode was changed by the
+ * chmod call
+ * @param success whether or not the chmod call was successful
+ */
 void print_verbose_message(const char *file_name, mode_t old_mode,
                            mode_t new_mode, bool changed, int success);
 
-#endif  // SRC_VERBOSE_VERBOSE_H_
+#endif // SRC_VERBOSE_VERBOSE_H_
