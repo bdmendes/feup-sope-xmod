e9d5d946dde22f2283ddd73367e86d63e1ee71f5
eukia <ritam.mendes@gmail.com>
2021-03-18 00:25:23 +0000

    Fix parsers formatting

--- src/parse/parsers.c
+++ src/parse/parsers.c
@@ -8,29 +8,28 @@
 #include "parsers.h"
 
 /**
- * @brief Assembles, from an octal number, a struct with the file permissions for an
- * user type.
+ * @brief Assembles, from an octal number, a struct with the file permissions
+ * for an user type.
  *
  * @param permissions struct to be filled
  * @param octal_mode octal number representing the permissions
  */
 static void assemble_permissions_user(PermissionTypes *permissions,
-                                      mode_t octal_mode)
-{
+                                      mode_t octal_mode) {
     permissions->read = octal_mode & 04;
     permissions->write = octal_mode & 02;
     permissions->execute = octal_mode & 01;
 }
 
 /**
- * @brief Assembles into a struct all the file permissions for the three user types.
+ * @brief Assembles into a struct all the file permissions for the three user
+ * types.
  *
  * @param file_permissions struct to be filled
  * @param octal_mode octal number representing the permissions
  */
 static void assemble_permissions(mode_t octal_mode,
-                                 FilePermissions *file_permissions)
-{
+                                 FilePermissions *file_permissions) {
     assemble_permissions_user(&file_permissions->other, octal_mode & 0007);
     assemble_permissions_user(&file_permissions->group,
                               (octal_mode & 0070) >> 3);
@@ -45,8 +44,7 @@ static void assemble_permissions(mode_t octal_mode,
  * @param changes flags to be added in the current permissions
  */
 static void add_permissions(PermissionTypes *curr_permissions,
-                            PermissionTypes *changes)
-{
+                            PermissionTypes *changes) {
     curr_permissions->read |= changes->read;
     curr_permissions->write |= changes->write;
     curr_permissions->execute |= changes->execute;
@@ -59,8 +57,7 @@ static void add_permissions(PermissionTypes *curr_permissions,
  * @param changes flags to be removed in the current permissions
  */
 static void remove_permissions(PermissionTypes *curr_permissions,
-                               const PermissionTypes *changes)
-{
+                               const PermissionTypes *changes) {
     if (changes->read)
         curr_permissions->read = false;
     if (changes->write)
@@ -78,19 +75,17 @@ static void remove_permissions(PermissionTypes *curr_permissions,
  */
 static void update_curr_permissions(PermissionTypes *curr_permissions,
                                     PermissionTypes *changes,
-                                    const char operator)
-{
-    switch (operator)
-    {
-    case '+':
-        add_permissions(curr_permissions, changes);
-        break;
-    case '-':
-        remove_permissions(curr_permissions, changes);
-        break;
-    case '=':
-        *curr_permissions = *changes;
-        break;
+                                    const char operator) {
+    switch (operator) {
+        case '+':
+            add_permissions(curr_permissions, changes);
+            break;
+        case '-':
+            remove_permissions(curr_permissions, changes);
+            break;
+        case '=':
+            *curr_permissions = *changes;
+            break;
     }
 }
 
@@ -101,8 +96,7 @@ static void update_curr_permissions(PermissionTypes *curr_permissions,
  * @param permissions current permissions
  */
 static void update_permissions(const char symbolic_changes[],
-                               FilePermissions *permissions)
-{
+                               FilePermissions *permissions) {
     int operator_index = is_permission_operator(symbolic_changes[0]) ? 0 : 1;
 
     PermissionTypes input_permissions;
@@ -110,34 +104,32 @@ static void update_permissions(const char symbolic_changes[],
     input_permissions.write = strchr(symbolic_changes, 'w') != NULL;
     input_permissions.execute = strchr(symbolic_changes, 'x') != NULL;
 
-    switch (symbolic_changes[0])
-    {
-    case 'u':
-        update_curr_permissions(&permissions->owner, &input_permissions,
-                                symbolic_changes[operator_index]);
-        break;
-    case 'g':
-        update_curr_permissions(&permissions->group, &input_permissions,
-                                symbolic_changes[operator_index]);
-        break;
-    case 'o':
-        update_curr_permissions(&permissions->other, &input_permissions,
-                                symbolic_changes[operator_index]);
-        break;
-    case 'a':
-    default:
-        update_curr_permissions(&permissions->owner, &input_permissions,
-                                symbolic_changes[operator_index]);
-        update_curr_permissions(&permissions->group, &input_permissions,
-                                symbolic_changes[operator_index]);
-        update_curr_permissions(&permissions->other, &input_permissions,
-                                symbolic_changes[operator_index]);
-        break;
+    switch (symbolic_changes[0]) {
+        case 'u':
+            update_curr_permissions(&permissions->owner, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            break;
+        case 'g':
+            update_curr_permissions(&permissions->group, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            break;
+        case 'o':
+            update_curr_permissions(&permissions->other, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            break;
+        case 'a':
+        default:
+            update_curr_permissions(&permissions->owner, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            update_curr_permissions(&permissions->group, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            update_curr_permissions(&permissions->other, &input_permissions,
+                                    symbolic_changes[operator_index]);
+            break;
     }
 }
 
-mode_t get_octal_mode(FilePermissions *permissions)
-{
+mode_t get_octal_mode(FilePermissions *permissions) {
     return permissions->owner.read * S_IRUSR |
            permissions->owner.write * S_IWUSR |
            permissions->owner.execute * S_IXUSR |
@@ -155,8 +147,7 @@ mode_t get_octal_mode(FilePermissions *permissions)
  * @param symbolic_mode symbolic mode
  * @param xmodCommand xmod command arguments
  */
-static void parse_symbolic_mode(char *symbolic_mode, XmodCommand *xmodCommand)
-{
+static void parse_symbolic_mode(char *symbolic_mode, XmodCommand *xmodCommand) {
     mode_t curr_mode;
     FileInfo file_info;
     retrieve_file_info(&file_info, xmodCommand->file_dir);
@@ -167,8 +158,7 @@ static void parse_symbolic_mode(char *symbolic_mode, XmodCommand *xmodCommand)
 
     const char sep[2] = ",";
     char *pos = NULL;
-    for (char *i = strtok_r(symbolic_mode, sep, &pos); i != NULL;)
-    {
+    for (char *i = strtok_r(symbolic_mode, sep, &pos); i != NULL;) {
         update_permissions(i, &curr_mode_permissions);
         i = strtok_r(NULL, sep, &pos);
     }
@@ -182,8 +172,7 @@ static void parse_symbolic_mode(char *symbolic_mode, XmodCommand *xmodCommand)
  * @param mode_str octal mode
  * @param xmodCommand xmod command arguments
  */
-static void parse_octal_mode(const char *mode_str, XmodCommand *xmodCommand)
-{
+static void parse_octal_mode(const char *mode_str, XmodCommand *xmodCommand) {
     mode_t mode = strtoul(mode_str, NULL, 8) & 0777;
     xmodCommand->octal_mode = mode;
 }
@@ -194,21 +183,18 @@ static void parse_octal_mode(const char *mode_str, XmodCommand *xmodCommand)
  * @param options options
  * @param xmodCommand xmod command arguments
  */
-static void parse_options(const char *options, XmodCommand *xmodCommand)
-{
+static void parse_options(const char *options, XmodCommand *xmodCommand) {
     xmodCommand->options.recursive |= strchr(options, 'R') != NULL;
     xmodCommand->options.verbose |= strchr(options, 'v') != NULL;
     xmodCommand->options.changes |=
         strchr(options, 'c') != NULL && !xmodCommand->options.verbose;
 }
 
-void parse(char **argv, XmodCommand *xmodCommand)
-{
+void parse(char **argv, XmodCommand *xmodCommand) {
     int mode_index = 1;
 
     memset(&xmodCommand->options, 0, sizeof(xmodCommand->options));
-    while (is_flag_arg(argv[mode_index]))
-    {
+    while (is_flag_arg(argv[mode_index])) {
         parse_options(argv[mode_index++], xmodCommand);
     }
 
@@ -216,12 +202,9 @@ void parse(char **argv, XmodCommand *xmodCommand)
              argv[mode_index + 1]);
     strip_trailing_slashes(xmodCommand->file_dir);
 
-    if (is_number_arg(argv[mode_index]))
-    {
+    if (is_number_arg(argv[mode_index])) {
         parse_octal_mode(argv[mode_index], xmodCommand);
-    }
-    else
-    {
+    } else {
         parse_symbolic_mode(argv[mode_index], xmodCommand);
     }
 
--- src/parse/parsers.h
+++ src/parse/parsers.h
@@ -1,49 +1,50 @@
 #ifndef SRC_PARSE_PARSERS_H_
 #define SRC_PARSE_PARSERS_H_
 
-#include <stdbool.h>
 #include <dirent.h>
+#include <stdbool.h>
 #include <sys/types.h>
 
 /**
-* @brief Struct which encapsulates the permissions' flags.
-*/ 
+ * @brief Struct which encapsulates the permissions' flags.
+ */
 typedef struct {
     /**
      * Read flag.
-    */
+     */
     bool read;
     /**
      * Write flag.
-    */
+     */
     bool write;
     /**
      * Execute flag.
-    */
+     */
     bool execute;
 } PermissionTypes;
 
 /**
-* @brief Struct which encapsulates all the permissions for the three user types.
-*/ 
+ * @brief Struct which encapsulates all the permissions for the three user
+ * types.
+ */
 typedef struct {
     /**
      * @brief Struct containing the owner permissions.
-    */
+     */
     PermissionTypes owner;
-     /**
+    /**
      * @brief Struct containing the group permissions.
-    */
+     */
     PermissionTypes group;
-     /**
+    /**
      * @brief Struct containing the other permissions.
-    */
+     */
     PermissionTypes other;
 } FilePermissions;
 
 /**
-* @brief Struct which encapsulates all arguments received by input.
-*/ 
+ * @brief Struct which encapsulates all arguments received by input.
+ */
 typedef struct {
     /**
      * @brief Octal mode.
@@ -56,15 +57,15 @@ typedef struct {
     struct {
         /**
          * @brief changes flag
-        */
+         */
         bool changes;
         /**
          * @brief verbose flag
-        */
+         */
         bool verbose;
         /**
          * @brief recursive flag
-        */
+         */
         bool recursive;
     } options;
 
@@ -80,19 +81,20 @@ typedef struct {
 } XmodCommand;
 
 /**
-* @brief Parses argv into a xmod command struct
-* 
-* @param argv argv
-* @param xmodCommand xmod command arguments
-*/
+ * @brief Parses argv into a xmod command struct
+ *
+ * @param argv argv
+ * @param xmodCommand xmod command arguments
+ */
 void parse(char **argv, XmodCommand *xmodCommand);
 
 /**
-* @brief Assembles an octal number from a struct with all the three users' permissions.
-* 
-* @param permissions struct with all the three users' permissions.
-* @return octal number
-*/
+ * @brief Assembles an octal number from a struct with all the three users'
+ * permissions.
+ *
+ * @param permissions struct with all the three users' permissions.
+ * @return octal number
+ */
 mode_t get_octal_mode(FilePermissions *permissions);
 
-#endif  // SRC_PARSE_PARSERS_H_
+#endif // SRC_PARSE_PARSERS_H_
